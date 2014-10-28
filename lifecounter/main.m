4051573ca4480ef6b4ee06291d1f70d5b07f41b3
apdg <gunn@Air.local>
2014-10-27 21:05:28 -0700

    Initial Commit

---
+++ lifecounter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lifecounter
+//
+//  Created by Adam Gunn on 10/27/14.
+//  Copyright (c) 2014 Adam Gunn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
